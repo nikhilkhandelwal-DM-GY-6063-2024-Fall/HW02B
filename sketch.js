70b9ebe5e9e8373c465617190bee599e618d31ef
nikhil0119 <nikhilkhandelwal0119@gmail.com>
2024-10-03 22:22:39 -0400

    rotation code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  background(236,209,161); // Got the RGB Code from color picker tool available online
+  background(236, 209, 161);
 
   let squareSize = 50;
   let padding = 10; // Space between squares
@@ -11,56 +11,136 @@ function setup() {
   let startY = (height - (squareSize * 4 + padding * 3)) / 2;
 
   // Row 1
-  fill(1,82,143);
-  rect(startX, startY, squareSize, squareSize);
-  
-  fill(1,82,143);
-  rect(startX + squareSize + padding, startY, squareSize, squareSize);
-  
-  fill(58,40,43);
-  rect(startX + (squareSize + padding) * 2, startY, squareSize, squareSize);
-  
-  fill(17,84,92);
-  rect(startX + (squareSize + padding) * 3, startY, squareSize, squareSize);
-  
+  fill(1, 82, 143);
+  push();
+  translate(startX + squareSize / 2, startY + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(1, 82, 143);
+  push();
+  translate(startX + (squareSize + padding) + squareSize / 2, startY + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(58, 40, 43);
+  push();
+  translate(startX + (squareSize + padding) * 2 + squareSize / 2, startY + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(17, 84, 92);
+  push();
+  translate(startX + (squareSize + padding) * 3 + squareSize / 2, startY + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
   // Row 2
-  fill(1,82,143);
-  rect(startX, startY + squareSize + padding, squareSize, squareSize);
-  
-  fill(0,0,128);
-  rect(startX + squareSize + padding, startY + squareSize + padding, squareSize, squareSize);
-  
-  fill(0,0,128);
-  rect(startX + (squareSize + padding) * 2, startY + squareSize + padding, squareSize, squareSize);
-  
-  fill(1,82,143);
-  rect(startX + (squareSize + padding) * 3, startY + squareSize + padding, squareSize, squareSize);
-  
-  // Row 3
-  fill(17,84,92);
-  rect(startX, startY + (squareSize + padding) * 2, squareSize, squareSize);
-  
-  fill(58,40,43);
-  rect(startX + squareSize + padding, startY + (squareSize + padding) * 2, squareSize, squareSize);
-  
-  fill(1,82,143);
-  rect(startX + (squareSize + padding) * 2, startY + (squareSize + padding) * 2, squareSize, squareSize);
-  
+  fill(1, 82, 143);
+  push();
+  translate(startX + squareSize / 2, startY + (squareSize + padding) + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
   fill(0, 0, 128);
-  rect(startX + (squareSize + padding) * 3, startY + (squareSize + padding) * 2, squareSize, squareSize);
-  
+  push();
+  translate(startX + (squareSize + padding) + squareSize / 2, startY + (squareSize + padding) + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(0, 0, 128);
+  push();
+  translate(startX + (squareSize + padding) * 2 + squareSize / 2, startY + (squareSize + padding) + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(1, 82, 143);
+  push();
+  translate(startX + (squareSize + padding) * 3 + squareSize / 2, startY + (squareSize + padding) + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  // Row 3
+  fill(17, 84, 92);
+  push();
+  translate(startX + squareSize / 2, startY + (squareSize + padding) * 2 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(58, 40, 43);
+  push();
+  translate(startX + (squareSize + padding) + squareSize / 2, startY + (squareSize + padding) * 2 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(1, 82, 143);
+  push();
+  translate(startX + (squareSize + padding) * 2 + squareSize / 2, startY + (squareSize + padding) * 2 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(0, 0, 128);
+  push();
+  translate(startX + (squareSize + padding) * 3 + squareSize / 2, startY + (squareSize + padding) * 2 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
   // Row 4
-  fill(1,82,143);
-  rect(startX, startY + (squareSize + padding) * 3, squareSize, squareSize);
-  
-  fill(0,0,128);
-  rect(startX + squareSize + padding, startY + (squareSize + padding) * 3, squareSize, squareSize);
-  
-  fill(0,0,128);
-  rect(startX + (squareSize + padding) * 2, startY + (squareSize + padding) * 3, squareSize, squareSize);
-  
-  fill(17,84,92);
-  rect(startX + (squareSize + padding) * 3, startY + (squareSize + padding) * 3, squareSize, squareSize);
+  fill(1, 82, 143);
+  push();
+  translate(startX + squareSize / 2, startY + (squareSize + padding) * 3 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(0, 0, 128);
+  push();
+  translate(startX + (squareSize + padding) + squareSize / 2, startY + (squareSize + padding) * 3 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(0, 0, 128);
+  push();
+  translate(startX + (squareSize + padding) * 2 + squareSize / 2, startY + (squareSize + padding) * 3 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
+
+  fill(17, 84, 92);
+  push();
+  translate(startX + (squareSize + padding) * 3 + squareSize / 2, startY + (squareSize + padding) * 3 + squareSize / 2);
+  rotate(random(360));
+  rectMode(CENTER);
+  rect(0, 0, squareSize, squareSize);
+  pop();
 }
 
 function draw() {
